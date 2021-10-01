b64a01b6e16058369d3b2b45c9d32003c645207e
aslukyanov <andrey.lukianov89@gmail.com>
2021-10-01 02:30:48 -0700

    some fix

--- homework_02/car.py
+++ homework_02/car.py
@@ -18,20 +18,23 @@ from homework_02.base import Vehicle
 from homework_02.engine import Engine
 
 class Car(Vehicle) :
+    engine = None
+    def set_engine(self, engine) :
+        #self.engine = Engine(volume, pistons)
+        self.engine = engine
 
-    def set_engine(self, volume, pistons) :
-        engine = Engine(volume, pistons)
+if __name__ == "__main__" :
+    car = Car(0,0,0)
+    #print(car.engine)
+    #car.set_engine(1,2)
+    #print(car.engine)
+    #print(car.engine.volume)
+    #print(car.engine.pistons)
 
-
-
-
-
-
-
-
-
-
-
+    engine = Engine(100, 6)
+    car.set_engine(engine)
+    print(car.engine.volume)
+    print(car.engine.pistons)
 
 
 
