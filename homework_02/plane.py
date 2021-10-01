bf348a388d7bea0af9e3f1c6d6add846f8b8a009
aslukyanov <andrey.lukianov89@gmail.com>
2021-10-01 03:14:40 -0700

    all tests were passed

--- homework_02/plane.py
+++ homework_02/plane.py
@@ -12,8 +12,10 @@ from homework_02 import exceptions
 from homework_02.base import Vehicle
 
 
+
+
 class Plane(Vehicle) :
-    def __init__(self, max_cargo, weight=0, fuel=0, fuel_consumption=0, cargo=0) :
+    def __init__(self, weight=0, fuel=0, fuel_consumption=0, max_cargo=0, cargo=0) :
         super().__init__(weight, fuel, fuel_consumption)
         self.cargo = cargo
         self.max_cargo = max_cargo
@@ -32,16 +34,16 @@ class Plane(Vehicle) :
 
 if __name__ == "__main__" :
     #my_plane = Plane(200, weight=2000, cargo=50)
-    my_plane = Plane(200, cargo=50)
+    #my_plane = Plane(200, cargo=50)
     #print(my_plane.fuel)
     #print(my_plane.max_cargo)
     #print(my_plane.cargo)
-    my_plane.cargo = 100
-    print(my_plane.cargo)
-    my_plane.load_cargo(100)
-    print(my_plane.cargo)
-    my_plane.load_cargo(100)
-    print(my_plane.cargo)
+    #my_plane.cargo = 100
+    #print(my_plane.cargo)
+    #my_plane.load_cargo(100)
+    #print(my_plane.cargo)
+    #my_plane.load_cargo(100)
+    #print(my_plane.cargo)
 
     #plane.remove_all_cargo())
     #print(my_plane.weight)
@@ -52,6 +54,25 @@ if __name__ == "__main__" :
 
     #my_plane.weight = 2000
     #print(my_plane.weight)
+
+    from faker import Faker
+
+    fake = Faker()
+
+    weight = fake.pyint()
+    print("weight=", weight)
+    fuel = fake.pyint()
+    #print("fuel=", fuel)
+    fuel_consumption = fake.pyint()
+    #print("fuel_consumption=", fuel_consumption)
+    max_cargo = fake.pyint(1000, 50000)
+    #print("max_cargo=", max_cargo)
+    plane = Plane(weight, fuel, fuel_consumption, max_cargo)
+    print("assigned weight =", plane.weight)
+
+    assert plane.weight == weight
+
+
 
 
 
