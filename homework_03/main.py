3fcc270016936eb6b543ccfbe256d54fad422f74
aslukyanov <andrey.lukianov89@gmail.com>
2021-10-17 12:48:37 -0700

    v2

--- homework_03/main.py
+++ homework_03/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 
 from fastapi.templating import Jinja2Templates
-from shed import get_my_shed
+from shed import get_my_shed, get_period_of_sched
 
 app = FastAPI()
 templates = Jinja2Templates(directory="./template")
@@ -18,17 +18,29 @@ def home():
 
 @app.get("/my_sched/{page_name}", response_class=HTMLResponse)
 def page(request: Request, page_name: str):
-    month = int(page_name.split("_")[1])
-    day = int(page_name.split("_")[0])
-    responce_date = page_name.split("_")[0] + "." + page_name.split("_")[1]
-    data = {
-        "shift": get_my_shed(day, month)[0],
-        "day" : get_my_shed(day, month)[1],
-        "responce" : responce_date
-    }
-    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+    if len(page_name.split("_")) == 2 :
 
+        month = int(page_name.split("_")[1])
+        day = int(page_name.split("_")[0])
+        responce_date = page_name.split("_")[0] + "." + page_name.split("_")[1]
+        data = {
+            "shift": get_my_shed(day, month)[0],
+            "day" : get_my_shed(day, month)[1],
+            "responce" : responce_date
+        }
+        return templates.TemplateResponse("page.html", {"request": request, "data": data})
 
+    if len(page_name.split("_")) == 4 :
+        month1 = int(page_name.split("_")[1])
+        day1 = int(page_name.split("_")[0])
+        month2 = int(page_name.split("_")[3])
+        day2 = int(page_name.split("_")[2])
+
+        data = {
+            "result": get_period_of_sched(day1, month1, day2, month2)
+        }
+        print(data)
+        return templates.TemplateResponse("page2.html", {"request": request, "data": data})
 
 
 
--- homework_03/shed.py
+++ homework_03/shed.py
@@ -9,9 +9,36 @@ def get_my_shed(day, month) :
     expected_day = int(f"{expected_date:%j}")
     return my_duty_week[expected_day - 1], expected_date.strftime('%A')
 
+def get_period_of_sched(day1, month1, day2, month2) :
+    return_lst = []
+    start_date = datetime.date(2021, month1, day1)
+    start_day = int(f"{start_date:%j}")
+
+    end_date = datetime.date(2021, month2, day2)
+    end_day = int(f"{end_date:%j}")
+    for i in range(start_day, end_day + 1) :
+        current_date = datetime.datetime.strptime("2021" + "-" + str(i), "%Y-%j").strftime("%d-%m")
+        current_day = datetime.date(2021, int(current_date.split("-")[1]), int(current_date.split("-")[0])).strftime('%A')
+        #print(current_date, current_day, my_duty_week[i - 1])
+        result = current_date + " is " + current_day + " - " + my_duty_week[i - 1]
+        return_lst.append(result)
+
+    return return_lst
 
 
 if __name__ == "__main__" :
-    print(get_my_shed(20, 10))
+    #print(get_my_shed(20, 10))
+    print(get_period_of_sched(16, 10, 26, 10))
+
+
+
+
+
+
+
+
+
+
+
 
 
