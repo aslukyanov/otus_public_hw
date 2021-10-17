154c188f743c21a2473f66c7d463382dfec46620
aslukyanov <andrey.lukianov89@gmail.com>
2021-10-17 09:37:42 -0700

    test

--- homework_03/main.py
+++ homework_03/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-import asyncio
 
 from fastapi.templating import Jinja2Templates
 from shed import get_my_shed
@@ -9,21 +8,18 @@ app = FastAPI()
 templates = Jinja2Templates(directory="./template")
 
 @app.get("/")
-async def home():
+def home():
     data = {
         "name": "My 2021 schedule",
-        "ex": "Go to /my_sched/gay_month page"
+        "ex": "Go to /my_sched/day_month page"
     }
     return {"data": data}
 
 
 @app.get("/my_sched/{page_name}", response_class=HTMLResponse)
-async def page(request: Request, page_name: str):
-
-    
+def page(request: Request, page_name: str):
     month = int(page_name.split("_")[1])
     day = int(page_name.split("_")[0])
-
     data = {
         "page": get_my_shed(day, month)
     }
